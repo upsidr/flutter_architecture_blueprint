1f558eb541eaf78a8efee8fbc82fb22f625b85ba
yu6hooke <17083696+yu6hooke@users.noreply.github.com>
2024-03-12 11:19:57 +0900

    Add device_preview

---
+++ lib/main_device_preview.dart
@@ -0,0 +1,19 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_architecture_blueprint/main.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+Future<void> main() async {
+  await initializeDateFormatting();
+  Intl.defaultLocale = 'ja_JP';
+
+  runApp(
+    DevicePreview(
+      builder: (_) => const ProviderScope(
+        child: MyApp(),
+      ),
+    ),
+  );
+}
